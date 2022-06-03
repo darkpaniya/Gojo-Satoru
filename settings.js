b748257d02fd48fa3b46a9daa3ac165c196ca151
darkpaniya <106427954+darkpaniya@users.noreply.github.com>
2022-06-03 20:23:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94778428985']
+global.premium = ['94778428985']
+global.ownernomer = '94778428985'
+global.ownername = '𝘋ᵃʳᵏ ₚₐₙᵢYₐ'
+global.botname = 'Qᵘᵉᵉⁿ ᴰᵉˢᴵ₂₀₂₂ 𝒩𝑒𝓌 💕'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCxJg9wdYzbHSbPdHkZJQeeg'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = 'Rusira'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
